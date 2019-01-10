35a690b64149a85a9a14bed2a557f7c0447b630a
sameerxD <samdragneal@gmail.com>
2019-01-10 10:16:18 -0800

    my 1st exercise

---
+++ charvalue.java
@@ -0,0 +1,13 @@
+public class charvalue {
+
+    public static void main(String[] args) {
+
+        char ch = 'f';
+        int ascii = ch;
+        // You can also cast char to int
+        
+
+        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+       
+    }
+}
---
+++ rangedatatypes.java
@@ -0,0 +1,13 @@
+public class rangedatatypes {
+
+   public static void main(String[] args) {
+       
+
+      System.out.println("byte 1 byte  -128 to 127");
+       System.out.println("short 2 bytes  -32,768 to 32,767");
+        System.out.println("int 4 bytes -2,147,483,648 to 2,147,483,64");
+         System.out.println("long 8 bytes -9,223,372,036,854,775,808 to 9,223,372,036,854,775,80");
+          System.out.println("float 4 bytes 7 decimal digits");
+           System.out.println("double 8 bytes 16 decimal digits");
+            
+}}
